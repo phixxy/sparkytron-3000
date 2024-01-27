56eebe53e9c731ff16545123f8128e6342a2d8aa
phixxy <bottlecap@protonmail.com>
2024-01-27 13:28:18 -0800

    removed "" as a possible username to count

--- extensions/highscores.py
+++ extensions/highscores.py
@@ -39,7 +39,8 @@ class Highscores(commands.Cog):
                     if user not in user_message_counts and user != "" and len(user) <= 32:
                         user_message_counts[user] = 1
                     else:
-                        user_message_counts[user] += 1
+                        if user != "":
+                            user_message_counts[user] += 1
             except Exception as error:
                 self.bot.logger.exception("Error occurred in highscores")
         
@@ -90,7 +91,8 @@ class Highscores(commands.Cog):
                     if user not in user_message_counts and user != "" and len(user) <= 32:
                         user_message_counts[user] = 1
                     else:
-                        user_message_counts[user] += 1
+                        if user != "":
+                            user_message_counts[user] += 1
             except Exception as error:
                 self.bot.logger.exception("Error occurred in highscores_server")
 
