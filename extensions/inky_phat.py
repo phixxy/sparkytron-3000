0743b62eb343e33693cd5c2b18222cdab63ceaae
phixxy <bottlecap@protonmail.com>
2024-02-03 00:37:10 -0800

    added bg image

--- extensions/inky_phat.py
+++ extensions/inky_phat.py
@@ -34,7 +34,10 @@ class InkyScreen(commands.Cog):
 
     async def write_to_display(self, text: list):
         if text is not self.old_message:
-            image = Image.new("P", (self.display.WIDTH, self.display.HEIGHT), (self.display.BLACK))
+            try:
+                image = Image.open("data/inky/bg.png")
+            except:
+                image = Image.new("P", (self.display.WIDTH, self.display.HEIGHT), (self.display.BLACK))
             draw = ImageDraw.Draw(image)
             width = self.display.WIDTH
             height = self.display.HEIGHT
