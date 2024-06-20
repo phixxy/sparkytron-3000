b14debfb37930eea783ccec9ee8b24606364c697
phixxy <bottlecap@protonmail.com>
2024-06-19 20:59:29 -0700

    Good enough to merge!

--- sparkytron_webui.py
+++ sparkytron_webui.py
@@ -6,23 +6,21 @@ import sys
 from dotenv import load_dotenv
 from src.bot import bot
 from src.webui import flask_app
+from waitress import serve
 
-
-def run_flask_app(process):
-    flask_port = os.getenv("flask_port")
-    if not flask_port:
-        flask_port = '5000'
+def get_flask_app(process):
     flask_app.bot_process = process
     flask_app.secret_key = "woaoaoahaowhawoiahoahhhhhh"
-    flask_app.run(debug=True, use_reloader=False ,host='0.0.0.0', port=flask_port)
-    
-    
+    return flask_app
 
 def main():
     load_dotenv()
+    flask_port = os.getenv("flask_port")
+    if not flask_port:
+        flask_port = '5000'
     process = subprocess.Popen([sys.executable, "sparkytron3000.py"])
-    run_flask_app(process)
-
-
+    flask_app = get_flask_app(process)
+    serve(flask_app, host='0.0.0.0', port=flask_port)
+    
 if __name__ == "__main__":
     main()
