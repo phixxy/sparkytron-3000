7eee640b17f3c45a22152f81696d05f551ca98fb
Phixxy <bottlecap@protonmail.com>
2025-01-01 18:28:45 -0800

    now work with multiple usernames

--- cogs/rss_feeds.py
+++ cogs/rss_feeds.py
@@ -7,19 +7,22 @@ class RSSCog(BotBaseCog):
     def __init__(self, bot):
         super().__init__(bot)
         self.setup(__class__.__name__)
-        self.rss_url = 'https://secure.runescape.com/m=adventurers-log/rssfeed?searchName=Frozener'
-        self.last_item = None
+        self.rss_base_url = 'https://secure.runescape.com/m=adventurers-log/rssfeed?searchName='
+        self.usernames = ['Deadifyed', 'Frozener']
+        self.last_items = {'Deadifyed':None, 'Frozener':None}
         self.check_rss.start()
 
     @tasks.loop(minutes=1)
     async def check_rss(self):
-        feed = feedparser.parse(self.rss_url)
-        latest_item = feed.entries[0] if feed.entries else None
-        
-        if latest_item and latest_item.title != self.last_item:
-            self.last_item = latest_item.title
-            channel = self.bot.get_channel(895388842834673696)
-            await channel.send(f"New RSS Item: {latest_item.title} - {latest_item.link}")
+        for name in self.usernames:
+            rss_url = self.rss_base_url + name
+            feed = feedparser.parse(rss_url)
+            latest_item = feed.entries[0] if feed.entries else None
+            
+            if latest_item and latest_item.title != self.last_items[name]:
+                self.last_items[name] = latest_item.title
+                channel = self.bot.get_channel(895388842834673696)
+                await channel.send(f"New RSS Item: {latest_item.title} - {latest_item.link}")
     
     @check_rss.before_loop
     async def before_check_rss(self):
